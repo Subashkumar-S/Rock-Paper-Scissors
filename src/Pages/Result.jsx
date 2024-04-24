a71fdad41c9ff18210793db7f3c8454954e21dc5
Subashkumar-S <subashs2232@gmail.com>
2024-04-24 21:38:53 +0530

    completed result page

--- src/Pages/Result.jsx
+++ src/Pages/Result.jsx
@@ -1,10 +1,10 @@
 import Header from "../components/Header/Header"
 import ResultContainer from "../components/ResultContainer/ResultContainer"
 import Rules from "../components/Rules/Rules"
-
+import style from "./Result.module.scss"
 const Result = () => {
   return (
-    <div>
+    <div className={style.container}>
         <Header />
         <ResultContainer />
         <Rules />
--- src/components/ResultContainer/ResultContainer.jsx
+++ src/components/ResultContainer/ResultContainer.jsx
@@ -1,7 +1,22 @@
+import Choice from "../Choice/Choice";
+import style from "./ResultContainer.module.scss";
 
 const ResultContainer = () => {
   return (
-    <div>ResultContainer</div>
+    <div className={style.container}>
+      <div className={style.choiceContainer}>
+        <h4>You Picked</h4>
+        <Choice />
+      </div>
+      <div className={style.result}>
+        <p>You Won</p>
+        <button>play again</button>
+      </div>
+      <div className={`${style.choiceContainer} ${style.won}`}>
+        <h4>The HOuse Picked</h4>
+        <Choice />
+      </div>
+    </div>
   )
 }
 
